586ac8e4bd87b73df98c146b47167d503614a1f2
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-04-06 19:54:19 +0700

    add card

--- my-project/src/component/Projects.jsx
+++ my-project/src/component/Projects.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { projectsData } from "../assets/assets";
 
 const Projects = () => {
   return (
-    <div className="container mx-auto flex flex-col items-center p-14 md:px-20 lg:px-32 overflow-hidde" id="Projects">
-      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+    <div className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden" id="Projects">
+      <h1 className="text-2xl sm:text-4xl font-bold mb-4 text-center">
         Projects<span className="font-light underline underline-offset-10 decoration-1 ml-1">Complated</span>
       </h1>
-      <p className="text-gray-500 max-w-80 text-center mb-8">Crafting Spaces, Building Legacies—Explore Our Portfolio</p>
-      <div className="">
-        <button><img src={assets.left_arrow} alt="" /></button>
-        <button><img src={assets.right_arrow} alt="" /></button>
+      <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">Crafting Spaces, Building Legacies—Explore Our Portfolio</p>
+      {/* Buttons */}
+      <div className="flex gap-2 justify-end flex-row items-end">
+        <button className="bg-gray-200 p-2 rounded">
+          <img src={assets.left_arrow} alt="" />
+        </button>
+        <button className="bg-gray-200 p-2 rounded">
+          <img src={assets.right_arrow} alt="" />
+        </button>
       </div>
-      <div></div>
+      {/* Projects Card */}
+      <div className="flex">
+        {projectsData.map((Project, index) => (
+          <div className="bg-white rounded-xl shadow p-4" key={index}>
+            <img src={Project.image} alt={Project.title} className="w-full h-48 object-cover" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
