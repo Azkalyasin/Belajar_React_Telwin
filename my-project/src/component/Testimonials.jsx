b46a42a8d056100d6c9bdc668eba292ab3ce4a2e
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-04-07 20:44:27 +0700

    make testiomonia;

---
+++ my-project/src/component/Testimonials.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="flex flex-col items-center justify-center container max-auto p-14 md:px-20 lg:px-32 overflow-hidden">
+      <h1  className="font-bold md:text-4xl text-2xl">
+        Projects
+        <span className="underline underline-offset-6 font-light ml-1 decoration-1">
+            Complated
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mx-auto mt-2 max-w-80">Real Stories from Those Who Found Home with Us</p>
+    </div>
+  );
+};
+
+export default Testimonials;
